88c1def33c67d3ea07a56a6021cb535b082e37dd
Anthony Benavente <anthony.m.benavente@gmail.com>
2015-12-15 00:20:07 -0500

    Initialized project structure using ng-boilerplate

---
+++ src/app/about/about.js
@@ -0,0 +1,23 @@
+angular.module( 'agora.about', [
+  'ui.router',
+  'placeholders',
+  'ui.bootstrap'
+])
+
+.config(function config( $stateProvider ) {
+  $stateProvider.state( 'about', {
+    url: '/about',
+    views: {
+      "main": {
+        controller: 'AboutCtrl',
+        templateUrl: 'about/about.tpl.html'
+      }
+    },
+    data:{ pageTitle: 'Benavente - About' }
+  });
+})
+
+.controller( 'AboutCtrl', function AboutCtrl( $scope ) {
+})
+
+;
